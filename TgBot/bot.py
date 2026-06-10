cd26e4f14cbde39e5014964a5b3e12279c32a8a5
TgBot <mod.saleroot@gmail.com>
2026-06-10 20:00:55 +0300

    v1.4

--- TgBot/bot.py
+++ TgBot/bot.py
@@ -5,16 +5,21 @@ from dotenv import load_dotenv
 # pip install aiogram
 from aiogram import Bot, Dispatcher      # [1]
 from aiogram.types import Message        # [1]
+from aiogram.filters import Command      
 
 dp = Dispatcher()                        # [2]
 
 
-@dp.message()                            # [3]
-async def any_message(                   # [4]
-        message: Message,                # [5]
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("Let`s talk!")
+
+
+async def any_message(                  
+        message: Message,                
 ):
     print(f"{message.from_user.full_name}: {message.text}")
-    await message.answer("Hello world!") # [6]
+    await message.answer("Hello world!") 
 
 async def main():
     load_dotenv()
