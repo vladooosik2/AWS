fded56537a9e58c4b65ae185f832e815079ba440
TgBot <mod.saleroot@gmail.com>
2026-06-24 18:34:03 +0300

    fix

--- TgBot/db.py
+++ TgBot/db.py
@@ -1,9 +1,9 @@
-import boto3
+import boto3  
 
 class DataBase:
     def __init__(self, table_name):
         self.dynamodb = boto3.resource('dynamodb')
-        self.table = self.dynamodb.Table('users')
+        self.table = self.dynamodb.Table(table_name)
 
     def __str__(self):
         return str(self.table)
